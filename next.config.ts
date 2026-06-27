e9c2dfbb881db8d1a9c9e0f742a5f98bb7c50bd3
Jedryszczyk <rf6swd6jt4@privaterelay.appleid.com>
2026-06-27 02:19:24 +0100

    Include DuckDB native files for Overture worker

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@duckdb/node-api"],
+  outputFileTracingIncludes: {
+    "/*": [
+      "./node_modules/@duckdb/node-api/**/*",
+      "./node_modules/@duckdb/node-bindings/**/*",
+      "./node_modules/@duckdb/node-bindings-*/*",
+      "./node_modules/detect-libc/**/*",
+    ],
+  },
 };
 
 export default nextConfig;
